2a18c14c863fe992ccaba2176b644216331d73a4
Stephen Melrose <me@stephenmelrose.co.uk>
2014-07-21 15:33:59 +0100

    Migrated from Vagrant to decking

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ docker-multi-container-example
 ==============================
 
 Example of an application using multiple Docker containers.
+
+Currently in development. Docs will be fleshed out when I have in place what I'm doing.
---
+++ mysql/start.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+# If we have no mysql data, i.e. volume is empty
+if [ ! -f /var/lib/mysql/ibdata1 ]; then
+
+    # Init the mysql data
+    echo "Initialising mysql data directory..."
+    mysql_install_db
+
+    # Start mysql and wait for it to be running
+    echo "Starting mysql..."
+    /usr/bin/mysqld_safe &
+    mysql_pid=$!
+    until mysqladmin ping &>/dev/null; do
+        echo -n "."
+        sleep 0.2
+    done
+    echo ""
+
+    # Create the admin user
+    echo "Creating admin user..."
+    echo "GRANT ALL ON *.* TO admin@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
+
+    # Shutdown mysql and wait for it to stop
+    echo "Stopping mysql..."
+    mysqladmin shutdown
+    wait $mysql_pid
+fi
+
+/usr/bin/mysqld_safe
